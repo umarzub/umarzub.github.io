9edb09677f8e35b5635e3e0701262d0bbee51bdd
Umar Zubairi <engineerumarzubairi@gmail.com>
2024-09-21 17:20:24 +0500

    Added Links and Buttons

--- script.js
+++
@@ -1,4 +0,0 @@
-// Add an interactive alert when the button is clicked
-function showAlert() {
-    alert("Welcome to my AI and Machine Learning Portfolio!");
-}
---
+++ scripts.js
@@ -0,0 +1,17 @@
+// Add an interactive alert when the button is clicked
+function showAlert() {
+    alert("Welcome to my AI and Machine Learning Portfolio!");
+}
+
+
+function openLinkedin() {
+    window.open("https://www.linkedin.com/in/umar-zubairi");
+}
+
+function openWhatsapp() {
+    window.open("https://wa.me/923260941361");
+}
+
+function openEmail() {
+    window.open("mailto:engineerumarzubairi@gmail.com")
+}
